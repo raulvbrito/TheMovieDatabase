e164bec6f51b98a0b73ced22920a713b9eed8d25
Raul Brito <raulvbrito@gmail.com>
2019-06-24 13:07:03 -0300

    Initial Commit

---
+++ TheMovieDatabase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMovieDatabase
+//
+//  Created by Raul Brito on 24/06/19.
+//  Copyright © 2019 Raul Brito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
